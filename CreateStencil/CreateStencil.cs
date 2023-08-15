a6b75c9d3bf7de382e01d2999eecd56ab6cebc1b
weilaideyueding <87157835+weilaideyueding@users.noreply.github.com>
2023-08-15 17:10:35 +0800

    Update CreateStencil.cs

--- CreateStencil/CreateStencil.cs
+++ CreateStencil/CreateStencil.cs
@@ -8,13 +8,21 @@ public class CreateStencil : Editor
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(baseShaderPath,"URP_Base.shader");
     }
 
-    private const string postprocessFeaturePath = "Assets/Editor/CreateStencil/URP_Base.shader";
+    private const string postprocessFeaturePath = "Assets/Editor/CreateStencil/Postprocess_Feature.cs";
     [MenuItem("Assets/Create/My Stencil/PostprocessFeature", priority = 11)]
     static void CreatePostprocessFeature()
     {
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(postprocessFeaturePath,"PostprocessFeature.cs");
     }
+    
+    private const string baseVolumeComponent = "Assets/Editor/CreateStencil/BaseVolumeComponent.cs";
+    [MenuItem("Assets/Create/My Stencil/BaseVolumeComponent", priority = 12)]
+    static void CreateBaseVolumeComponent()
+    {
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(baseVolumeComponent,"BaseVolumeComponent.cs");
+    }
+    
+    
 }
-
 
 
